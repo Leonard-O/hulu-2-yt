03aa6bb2538221dc565ccd1a88c61018b92a0cb5
Leonard <realcodedevs@gmail.com>
2021-11-07 17:39:21 +0300

    Nav

--- utils/requests.js
+++ utils/requests.js
@@ -11,6 +11,34 @@ export default {
     },
     fetchActionMovies: {
         title: 'Action',
-        url: `/discover/movie?api_key=${API_KEY}&with_genre`,
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+    },
+    fetchComedyMovies: {
+        title: 'Comedy',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+    },
+    fetchHorrorMovies: {
+        title: 'Horror',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+    },
+    fetchMystery: {
+        title: 'Mystery',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=9648`,
+    },
+    fetchSciFi: {
+        title: 'Sci-Fi',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=878`,
+    },
+    fetchWestern: {
+        title: 'Western',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=37`,
+    },
+    fetchAnimation: {
+        title: 'Animation',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=16`,
+    },
+    fetchTV: {
+        title: 'TV Movie',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=10770`,
     },
 };
