57ae0aa875a4650a93a5f56cd9cad35a07fa71c4
Leonard <realcodedevs@gmail.com>
2021-11-07 15:54:58 +0300

    Created Header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 export default function Home() {
 	return (
@@ -9,7 +11,11 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<h1>Lets Build Hulu 2.0</h1>
+			<Header />
+
+			<Nav />
+
+			{/* Results */}
 		</div>
 	);
 }
